ac0e9439af8952db0bce6d393ec6b273bee8c442
Mehmet Dokuyucu <mehmet.dokuyucu@tepeguvenlik.com.tr>
2022-01-21 19:28:33 +0300

    Add project files.

---
+++ Deneme/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Deneme.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deneme.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult CreateBill(List<Product> productlist, string date1, int price1)
+        {
+
+            //breakpoint ile baktığımız zaman veriler geliyor.
+
+            //backend işlemleri..
+            //
+
+            return Json(true);
+        }
+    }
+}
